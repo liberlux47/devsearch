8f4c41f0a70c2f4a59353dee7dd7f7cbe786096e
Lucas Liberman <liberlux@startmail.com>
2025-06-27 18:15:59 -0400

    feat: added template rendering

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
 def projects(request): 
-    return render(request, 'projects/projects.html')
+    page = 'projects'
+    msg = 'Hello, you are on the projects page!'
+    return render(request, 'projects/projects.html', {'message': msg, 'page': page})
 
 def project(request, pk):
     return render(request, 'projects/single-project.html')
